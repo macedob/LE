905cc7351029aa13b8ba64632b24a9eb25a39d60
Guilherme Almeida <uc2020236588@student.uc.pt>
2023-11-30 17:04:46 +0000

    Coisas

--- public/mapa.js
+++ public/mapa.js
@@ -1,7 +1,18 @@
-let data = d3.json("areas.json").then(function (data) {
+d3.json("areas.json").then(function (data) {
   // Specify the dimensions of the chart.
   const width = 928;
   const height = 680;
+
+  let raio = 20;
+
+  // Create the SVG container.
+  let svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .attr("style", "max-width: 100%; height: auto;");
 
   // Specify the color scale.
   const color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -12,26 +23,24 @@ let data = d3.json("areas.json").then(function (data) {
   const nodes = data.nodes.map((d) => ({ ...d }));
 
   // Create a simulation with several forces.
-  const simulation = d3
+  let simulation = d3
     .forceSimulation(nodes)
+    .nodes(nodes)
+    /*
     .force(
       "link",
       d3.forceLink(links).id((d) => d.id)
     )
-    .force("charge", d3.forceManyBody())
+    */
+    .force(
+      "charge",
+      d3.forceCollide((d) => d.radius + 10)
+    )
     .force("x", d3.forceX())
     .force("y", d3.forceY());
 
-  // Create the SVG container.
-  const svg = d3
-    .create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", [-width / 2, -height / 2, width, height])
-    .attr("style", "max-width: 100%; height: auto;");
-
   // Add a line for each link, and a circle for each node.
-  const link = svg
+  let link = svg
     .append("g")
     .attr("stroke", "#999")
     .attr("stroke-opacity", 0.6)
@@ -40,7 +49,7 @@ let data = d3.json("areas.json").then(function (data) {
     .join("line")
     .attr("stroke-width", (d) => Math.sqrt(d.value));
 
-  const node = svg
+  let node = svg
     .append("g")
     .attr("stroke", "#fff")
     .attr("stroke-width", 1.5)
@@ -65,7 +74,17 @@ let data = d3.json("areas.json").then(function (data) {
       .attr("x2", (d) => d.target.x)
       .attr("y2", (d) => d.target.y);
 
-    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    node
+      .attr("cx", (d) => d.x)
+      .attr("cy", (d) => d.y)
+      .attr("r", (d) => d.radius);
+    simulation
+      .force(
+        "charge",
+        d3.forceCollide((d) => d.radius + 5)
+      )
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
   });
 
   // Reheat the simulation when drag starts, and fix the subject position.
@@ -73,6 +92,12 @@ let data = d3.json("areas.json").then(function (data) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     event.subject.fx = event.subject.x;
     event.subject.fy = event.subject.y;
+    for (let i = 0; i < data.nodes.length; i++) {
+      if (data.nodes[i].id == event.subject.id) {
+        data.nodes[i].radius += 1;
+        event.subject.radius += 1;
+      }
+    }
   }
 
   // Update the subject (dragged node) position during drag.
@@ -84,15 +109,7 @@ let data = d3.json("areas.json").then(function (data) {
   // Restore the target alpha so the simulation cools after dragging ends.
   // Unfix the subject position now that it’s no longer being dragged.
   function dragended(event) {
-    if (!event.active) simulation.alphaTarget(0);
     event.subject.fx = null;
     event.subject.fy = null;
   }
-
-  // When this cell is re-run, stop the previous simulation. (This doesn’t
-  // really matter since the target alpha is zero and the simulation will
-  // stop naturally, but it’s a good practice.)
-  invalidation.then(() => simulation.stop());
-
-  return svg.node();
 });
